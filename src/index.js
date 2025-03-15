22c63b9f162ced5e639624a673583c5c491e951d
Joy Njoroge <njorogejoy78@gmail.com>
2025-03-15 23:49:48 +0300

    added form

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById("create-task-form");
+  const input = document.getElementById("new-task-description");
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault(); // prevents page reload
+    const taskText = input.value;
+
+    if (taskText.trim() !== "") {
+      addTaskToDOM(taskText);
+      form.reset(); // clears input field
+    }
+  });
 });
+
+function addTaskToDOM(task) {
+  const li = document.createElement("li");
+  li.textContent = task;
+
+  const ul = document.getElementById("tasks");
+  ul.appendChild(li);
+}
